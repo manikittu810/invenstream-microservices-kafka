794ae286856f02c4926ea2a0d546bb041c4a405d
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-12-20 19:35:09 -0800

    Initial commit

---
+++ src/main/java/com/inventstream/inventstream_project/InventstreamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.inventstream.inventstream_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventstreamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventstreamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventstream/inventstream_project/InventstreamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventstream.inventstream_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventstreamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
